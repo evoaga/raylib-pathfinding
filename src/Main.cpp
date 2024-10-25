e357bae12188beefc67e6b3b635ed3e656522ba5
agahakan <hakanqira@gmail.com>
2024-10-25 15:38:41 +0200

    l

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,23 @@
-#include "Engine.hpp"
+#include <iostream>
+#include "Color.hpp"
+#include "Image.hpp"
 
-#define fn auto
+int main()
+{    
+    Color red(1, 0, 0);
+    Color green(0, 1, 0);
+    Color black;
+    Color yellow = red + green;
 
-fn main() -> int { Engine::Run(); }
+    // Create an image in memory, and fill it with yellow
+    Image image(512, 512, yellow);
+
+    // Make a red square on the top left of the image
+    for (int y = 0; y < 100; y++) {
+      for (int x = 0; x < 100; x++) {
+        image.SetPixel(x, y, Color(1, 0, 0));
+      }
+    }
+
+    image.WriteFile("test.png");
+}
